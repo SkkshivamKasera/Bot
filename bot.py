593d33640be425baefef0174642cfc5e4f180400
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2023-08-07 11:03:00 +0530

    U

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import telebot
 import re
+import threading
 
 # Set your Telegram bot token here
 TELEGRAM_BOT_TOKEN = "6418563359:AAEO4WdB-ksRAfFlX9GC-d9bzrG6HnrYbBc"
 
 # Initialize the Telegram bot
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+# Initialize a lock
+api_lock = threading.Lock()
 
 # Global dictionary to store the new caption text provided by users for each video
 video_replacement_texts = {}
@@ -27,8 +31,11 @@ def handle_video(message):
                 # Add '[@captain_moviess]' to the beginning of the caption
                 updated_caption = "[@captain_moviess] " + current_caption
 
-            # Send the video back with the updated caption
-            bot.send_video(message.chat.id, message.video.file_id, caption=updated_caption)
+            # Store the updated caption in the global dictionary
+            video_replacement_texts[message.video.file_id] = updated_caption
+
+            # Send the video with the updated caption using the locked function
+            send_video_with_caption(message.chat.id, message.video.file_id, updated_caption)
 
         else:
             bot.send_message(message.chat.id, "Sorry, the original message is not a video with a caption, and I cannot proceed.")
@@ -36,6 +43,14 @@ def handle_video(message):
     except Exception as e:
         print(f"Error processing video: {e}")
         bot.reply_to(message, "Sorry, there was an error processing the video.")
+
+# Function to interact with the Telegram API
+def send_video_with_caption(chat_id, video_id, caption):
+    with api_lock:
+        try:
+            bot.send_video(chat_id, video_id, caption=caption)
+        except Exception as e:
+            print(f"Error sending video: {e}")
 
 # Polling to keep the bot running
 bot.polling()
